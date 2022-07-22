8be021de1c9e4754cfc900b72bacf16f2af21ecf
ronnoc710 <105008290+ronnoc710@users.noreply.github.com>
2022-07-22 17:18:11 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,13 +2,13 @@
 
 export const geoApiOptions = {
 	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '8b194f9185mshc61c4bb384b91d6p136bf9jsnd27585001d54',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
+	// headers: {
+	//	'X-RapidAPI-Key': '//* Insert API key *//',
+	//	'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	//}
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
-export const WEATHER_API_KEY = "2fb0513f89197ed46ffc005fa9946eb3"
+// export const WEATHER_API_KEY = "//* Insert API key *//"
